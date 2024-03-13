b9796f30ccfe5d16321c611b7d76aaa99c995164
Jason Johnson <145226961+JJohnson606@users.noreply.github.com>
2024-03-13 18:50:36 +0000

    added policy to user_controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,32 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show liked feed followers following discover ]
+  before_action :set_user, only: %i[show liked feed followers following discover]
+  before_action :authorize_user, only: [:show, :edit, :feed, :liked, :discover, :update]
 
-  private
+  def show  
+  end
+  
+  def edit
+     
+  end
+
+  def feed
+  end
+  
+  def discover
+  end
+
+  def liked
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: 'User was successfully updated.'
+    else
+      render :edit
+    end
+  end
+  
+private
 
     def set_user
       if params[:username]
@@ -10,4 +35,9 @@ class UsersController < ApplicationController
         @user = current_user
       end
     end
+
+    def authorize_user
+      authorize @user
+    end
+
 end
